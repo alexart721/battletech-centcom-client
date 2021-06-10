c77aadaa10d8e105203ac6e527d608b917b05871
Alex Teodorescu <ateodorescu721@gmail.com>
2021-06-10 16:29:03 -0400

    testing vercel again

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-
-let store = createStore();
-
-ReactDOM.render(
-  <Provider store={store}>
-    <h2>Welcome to Battletech!</h2>
-  </Provider>
-);
+export default function Home() {
+  return (<h2>Welcome to Battletech!</h2>);
+}
