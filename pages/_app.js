be762669f71d04af4aedfef6b4ca0c835fe4742c
Alex Teodorescu <ateodorescu721@gmail.com>
2021-06-13 12:22:58 -0400

    add redux store

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,17 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from '../lib/redux';
 import '../styles.css';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp ({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  // const store = configureStore({
+  //   tasks
+  // });
+
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
+};
